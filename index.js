14e3b476959ee676a0d22eaf1923411a50e5f2da
Shawna Carey <scareycs@gmail.com>
2021-07-03 13:01:03 -0400

    json file added

--- index.js
+++ index.js
@@ -7,18 +7,25 @@
 //it needs to be smart though.. did I choose this item already? How do go about saving this to a server? login accounts? Save my data!
 
 
-
-
-
-
 $(document).ready(function() {
-    var ListArray = ["For mason jar options", "Watercolor", "Sketching people", "Acrylic painting class", "Drive to thrift store", "Drive to decor store", "Rasberry PI Get home cloud working", "Rasberry PI Get web server working", "Work on power app certification", "Read a book", "Call somebody", "Text somebody", "Walk to mall/ home workout / yoga if bad weather", "Just walk", "Write a thank you letter", "Free write", "Write a poem", "Drive to michaels for string and needle for paper cranes project", "Paper crane project", "Water color paint something for bedroom frame", "Find a job you want to apply to and cater your resume", "Walk to art gallery", "Work on web version of this system", "Take a trip to oak grove", "Play switch"];
 
-    var item = ListArray[Math.floor(Math.random()*ListArray.length)];
+    fetch('listitems.json').then(function(response){
+        return response.json();
+    }).then(function(json) {
+        let items = json;
+        initialize(items);
+    }).catch(function(err){
+        console.log('Fetch problem: ' + err.message);
+    });
 
-    console.log(item);
+    function initialize(all) {
+        console.log(all);
+    }
+    //var item = ListArray[Math.floor(Math.random()*ListArray.length)];
 
-    html
+    
+
+
 });
 
 
